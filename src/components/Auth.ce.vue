8a9192a3549aad59a3dc3680a688de0df294744f
Ron Snyder <ron@snyderjr.com>
2023-07-24 16:06:01 -0400

    Auth testing

--- src/components/Auth.ce.vue
+++ src/components/Auth.ce.vue
@@ -1,7 +1,20 @@
 <template>
 
   <div ref="root">
-    <div @click="triggerNetlifyIdentityAction('login')">login</div>
+
+    <sl-tooltip v-if="isLoggedIn" :content="`Logged in as ${user?.email}`" placement="bottom">
+      <div @click="logout">Logout</div>
+    </sl-tooltip>
+
+    <sl-tooltip v-else content="Login" placement="bottom">
+      <div @click="login">Login</div>
+    </sl-tooltip>
+
+    <!--
+    <div v-if="isLoggedIn" @click="triggerNetlifyIdentityAction('logout')">logout</div>
+    <div v-else @click="triggerNetlifyIdentityAction('login')">login</div>
+    -->
+
   </div>
 
 </template>
@@ -10,7 +23,10 @@
 
   import { computed, onMounted, ref, toRaw, watch } from 'vue'
 
+  import '@shoelace-style/shoelace/dist/components/tooltip/tooltip.js'
+
   import netlifyIdentity from "netlify-identity-widget";
+  import jwt_decode from 'jwt-decode'
 
   import { useEntitiesStore } from '../store/entities'
   import { storeToRefs } from 'pinia'
@@ -18,50 +34,80 @@
   const store = useEntitiesStore()
   const { user } = storeToRefs(store)
 
-  const props = defineProps({
-  })
-
   const root = ref<HTMLElement | null>(null)
-  const shadowRoot = computed(() => root?.value?.parentNode as HTMLElement)
 
   onMounted(async () => {
-    let _user = localStorage.getItem('gotrue.user')
+    validate()
+    let _user: any = localStorage.getItem('gotrue.user')
     if (_user) {
       store.$state.user = JSON.parse(_user)
+      if (user.value) console.log('auth.omMounted', user.value, new Date(user.value.expires_at))
     }
-    console.log('auth.omMounted', user.value)
     netlifyIdentity.init({
       APIUrl: "https://juncture-search.netlify.app/.netlify/identity",
       logo: false
     })
   })
 
+  const isLoggedIn = computed(() => user.value != null)
+
   watch(user, () => {
-    console.log('auth.user', user.value, JSON.parse(localStorage.getItem('gotrue.user')))
+    console.log('auth.user', toRaw(user.value))
+    if (user.value) localStorage.setItem('user', JSON.stringify(user.value))
+    else if (localStorage.getItem('user')) localStorage.removeItem('user')
   })
 
-  function triggerNetlifyIdentityAction(action) {
-    console.log(`triggerNetlifyIdentityAction(${action})`)
-    if (action == 'login' || action == 'signup') {
-      netlifyIdentity.open(action)
-      netlifyIdentity.on(action, user => {
-        store.$state.user = {
-          username: user.user_metadata.full_name,
-          email: user.email,
-          access_token: user.token.access_token,
-          expires_at: user.token.expires_at,
-          refresh_token: user.token.refresh_token,
-          token_type: user.token.token_type
-        }
-        netlifyIdentity.close()
-      })
-    } else if (action == 'logout') {
-      store.$state.user = null
-      netlifyIdentity.logout()
-    }
+  function login() {
+    netlifyIdentity.open('login')
+    netlifyIdentity.on('login', _user => {
+      store.$state.user = _user
+      netlifyIdentity.close()
+    })
   }
+
+  function logout() {
+    store.$state.user = null
+    netlifyIdentity.logout()
+  }
+
+  function tokenIsValid(expiration:number) {
+    let isExpired = expiration <= Date.now()
+    console.log(`tokenIsValid=${!isExpired}`)
+    return !isExpired
+  }
+
+  function validate() {
+    let _user: any = localStorage.getItem('user') && JSON.parse(localStorage.getItem('user') || '{}' )
+    if (!_user) return Promise.resolve(null)
+    if (!tokenIsValid(_user.token.expires_at)) {
+      // keep users logged in
+      console.log('refreshing token')
+      const formData = new FormData()
+      formData.append('grant_type', 'refresh_token')
+      formData.append('refresh_token', _user.token.refresh_token)
+      fetch('https://juncture-search.netlify.app/.netlify/identity/token', {
+        method : 'POST',
+        body : formData
+      }).then(x=>x.json()).then((newToken: any) => {
+        console.log('newToken', newToken)
+        _user.token.access_token=newToken.access_token
+        _user.token.refresh_token=newToken.refresh_token
+        _user.token.expires_at = (jwt_decode(newToken.access_token) as any).exp * 1000
+        localStorage.setItem('user', JSON.stringify(_user))
+        store.$state.user = _user
+      })
+      return null
+      }
+      return null
+    }
+
 
 </script>
 
 <style>
+:host {
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+}
 </style>
